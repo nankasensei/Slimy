67be395d9e51896bdf615fc432b15a0a470ce25d
mashiro1204 <c4pump@gmail.com>
2020-03-07 03:17:32 +0900

    add win UI

--- Slimy03/Assets/Scripts/Items/Mushroom.cs
+++ Slimy03/Assets/Scripts/Items/Mushroom.cs
@@ -7,6 +7,6 @@ public class Mushroom : Item
     public override void Buff()
     {
         PlayerController player = GameObject.Find("Player").GetComponent<PlayerController>();
-        player.SetAtk(1.0f, 8.0f);
+        player.SetAtk(1.0f, 10.0f);
     }
 }
